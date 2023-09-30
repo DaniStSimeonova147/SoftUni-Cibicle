a756368be59f45973a8d09a47edde8147f7e2343
Dani Simeonova <danisimeonova147@gmail.com>
2023-09-30 18:00:16 +0300

    Add handlebars configuration

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+
 const expressConfig = require('./config/expressConfig');
+const handlebarsConfig = require('./config/handlebarsConfig');
 
 const app = express();
 const PORT = 5000;
 
 expressConfig(app);
+handlebarsConfig(app);
 
-//  Handlebars config
-app.engine('hbs', handlebars.engine({
-    extname: 'hbs',
-}))
-app.set('view engine', 'hbs');
-app.set('views', 'src/views');
 
 //  Routes
 app.get('/', (req, res) => {
