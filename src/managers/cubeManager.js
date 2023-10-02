10617c989e8e3936d398a345546c5591d1db8f09
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-02 17:49:28 +0300

    Add details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,24 @@
 const uniqid = require('uniqid');
-const cubes = [];
+const cubes = [
+    {
+        id: '44dx8nh1ln8zvrpz',
+        name: 'rubik',
+        description: 'It is cool!',
+        imageUrl: 'https://5.imimg.com/data5/SELLER/Default/2022/2/ZU/FH/UB/56427633/rubikscube.jpg',
+        difficultyLevel: 4,
+    },
+    {
+        id: '54dx8nh1ln8zvrpg',
+        name: 'rubik vol 2',
+        description: 'It is very cool!',
+        imageUrl: 'https://cuboss.com/wp-content/uploads/2021/04/stora-rubiks-kuber-storre-speedcubes.jpg',
+        difficultyLevel: 5,
+    }
+];
 
 exports.getAll = () => cubes.slice();
+
+exports.getOne = (cubeId) => cubes.find(x =>x.id ==cubeId);
 
 exports.create = (cubeData) => {
     const newCube = {
