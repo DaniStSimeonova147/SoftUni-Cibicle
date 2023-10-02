8a4a70bdf9d7b9cb04d2b75fe5631d2fe12b64b2
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-02 18:00:48 +0300

    Add 404 page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,15 +4,17 @@ const router = require('express').Router();
 const cubeManager = require('../managers/cubeManager');
 
 router.get('/', (req, res) => {
-const cubes = cubeManager.getAll();
+    const cubes = cubeManager.getAll();
 
-    res.render('index', {cubes});
+    res.render('index', { cubes });
 });
 
-router.get('/about', (req, res) =>{
+router.get('/about', (req, res) => {
     res.render('about');
 })
 
-
+router.get('/404', (req, res) => {
+    res.render('404');
+})
 
 module.exports = router;
