0dd42a7708d2f4be3fcaf37b825a61bb8d4e93fa
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-02 16:55:29 +0300

    Add cube manager

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const cubeManager = require('../managers/cubeManager');
 
 //  Path /cubes/create
 router.get('/create', (req, res) => {
@@ -6,8 +7,19 @@ router.get('/create', (req, res) => {
 });
 
 router.post('/create', (req, res) => {
-    console.log(req.body);
+    const { 
+        name,
+        description,
+        imageUrl,
+        difficultyLevel,
+    } = req.body;
 
+    cubeManager.create([
+        name,
+        description,
+        imageUrl,
+        difficultyLevel,
+    ]);
     res.redirect('/');
 });
 
