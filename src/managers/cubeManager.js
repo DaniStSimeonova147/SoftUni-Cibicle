3cafbac33f425005f9375272aabc6b937d932d90
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-02 18:17:17 +0300

    Add search

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -16,8 +16,20 @@ const cubes = [
     }
 ];
 
-exports.getAll = () => cubes.slice();
+exports.getAll = (search,from, to) => {
+   let result = cubes.slice();
 
+   if (search) {
+    result = result.filter(cube => cube.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+   }
+   if (from) {
+    result = result.filter(cube => cube.difficultyLevel >= Number(from));
+   }
+   if (to) {
+    result = result.filter(cube => cube.difficultyLevel <= Number(to));
+   }
+   return result;
+}
 exports.getOne = (cubeId) => cubes.find(x =>x.id ==cubeId);
 
 exports.create = (cubeData) => {
