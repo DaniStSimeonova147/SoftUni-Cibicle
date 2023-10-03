15f195032c59a2f958ab840a42f4e3e20387a50f
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-03 14:40:03 +0300

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
+const dbConnect = require('./config/dbConfig');
+const routes = require('./routes');
 
 const app = express();
 const PORT = 5000;
@@ -11,10 +11,10 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController);
-app.use('/cubes', cubeController);
-app.get('*', (req,res)=>{
-    res.redirect('/404');
-});
+dbConnect()
+.then(()=> console.log('DB Connescted successfully!'))
+.catch(err =>console.log('DB error: ', err));
+
+app.use(routes);
 
 app.listen(PORT, ()=> console.log(`Server is running on port ${PORT}...`));
