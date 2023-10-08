f68ec266f4e8d8e06ae60ae7045dbba74ad62c29
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-08 16:48:26 +0300

    Add register page

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -3,3 +3,5 @@ const Accesorry = require('../models/Accessory');
 exports.getAll = () => Accesorry.find();
 
 exports.create = (accessoryData) => Accesorry.create(accessoryData);
+
+exports.getOthers = (accessoryIds) => Accesorry.find({_id: {$nin: accessoryIds}});
