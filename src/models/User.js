bed29238a1dc483a4e06891428e546ddfb1ff81d
Dani Simeonova <danisimeonova147@gmail.com>
2023-10-11 12:56:06 +0300

    Validated username and passwords

--- src/models/User.js
+++ src/models/User.js
@@ -2,10 +2,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-    username: String,
+    username: {
+        type: String,
+        require: [true, 'Username is required!'],
+        minLength: [5, 'Username is too short!'],
+        match: [/^[A-Za-z0-9]+$/, 'Username must be alphanumeric!'],
+        unique: true,
+    },
     password: {
         type: String,
-
+        require: true,
+        minLength: [8, 'Password is too short!'],
+        validate: {
+            validator: function(value){
+                return /^[A-Za-z0-9]+$/.test(value);
+            },
+            message: 'Invalid password characters!'
+        }
     },
 });
 
@@ -16,11 +29,11 @@ userSchema.virtual('repeatPassword')
         }
     });
 
-    userSchema.pre('save', async function(){
-        const hash = await bcrypt.hash(this.password, 10);
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
 
-        this.password = hash;
-    });
+    this.password = hash;
+});
 
 const User = mongoose.model('User', userSchema);
 
